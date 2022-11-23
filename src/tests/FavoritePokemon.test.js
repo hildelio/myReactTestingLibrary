f8578cf8a7bcef7c50258ab2501790f85fb3dd5e
hildelio <hildelio@gmail.com>
2022-11-23 01:23:50 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+
+describe('Será avaliado se o arquivo teste FavoritePokemon.test.js contempla 100% dos casos de uso criados pelo Stryker', () => {
+  test('É exibido na tela a mensagem No favorite pokemon found', () => {
+    render(<FavoritePokemon />);
+    const pFavorite = screen.getByText(/no favorite pokémon found/i);
+    expect(pFavorite).toBeInTheDocument();
+  });
+});
