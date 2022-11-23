f029a1c14c17aafc60a5ca8b80d7d527f5484649
hildelio <hildelio@gmail.com>
2022-11-23 01:32:58 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Será avaliado se o arquivo teste NotFound.test.js contempla 100% dos casos de uso criados pelo Stryker', () => {
+  test('É exibido na tela um h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const h2NotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    expect(h2NotFound).toBeInTheDocument();
+  });
+  test('Existe uma imagem com o src https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const imgNotFound = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(imgNotFound.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
