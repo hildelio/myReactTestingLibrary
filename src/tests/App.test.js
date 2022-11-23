0d38ac6627f9a388957170692364d0c2583697d9
hildelio <hildelio@gmail.com>
2022-11-23 00:46:40 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => { a; });
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Será avaliado se o arquivo teste App.test.js contempla 100% dos casos de uso criados pelo Stryker', () => {
+  test('É exibido na tela um link com o texto Home', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    expect(homeLink).toBeInTheDocument();
+  });
+  test('É exibido na tela um link com o texto About', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    expect(aboutLink).toBeInTheDocument();
+  });
+  test('É exibido na tela um link com o texto Favorite Pokemón', () => {
+    renderWithRouter(<App />);
+    const favoritePokemonLink = screen.getByRole('link', { name: /favorite pokémon/i });
+    expect(favoritePokemonLink).toBeInTheDocument();
+  });
+});
